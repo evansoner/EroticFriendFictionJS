850321393bbfa1836fd9c0f6144e910832dc5e82
Jack Wong <jackwongau@hotmail.com>
2014-06-26 11:33:23 +1000

    Jack added word list
    Added words

--- js/ajax.js
+++ js/ajax.js
@@ -1,9 +1,9 @@
-function wordCheck( str, callback ){
+function wordCheck(str, callback) {
     var ajax=new XMLHttpRequest();
-    ajax.onreadystatechange=function(){
-	if (ajax.readyState==4 && ajax.status==200){
-	    callback(ajax.responseText);
-	}
+    ajax.onreadystatechange=function() {
+		if ((ajax.readyState == 4) && (ajax.status == 200)) {
+			callback(ajax.responseText);
+		}
     }
     ajax.open("POST","wordCheck.php",true);
     ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
